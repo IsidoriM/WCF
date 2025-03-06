3205c992032eb81dfc662c4528dfb25e5285e8e8
IsidoriM <isidorimassimiliano2@gmail.com>
2025-03-06 10:22:01 +0100

    Lesson 2 - Part 2 - Creating a remoting service and a web service

---
+++ WCF/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        IHelloRemotingService.IHelloRemtingService client;
+        TcpChannel channel = new TcpChannel();
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ChannelServices.RegisterChannel(channel);
+            client = (IHelloRemotingService.IHelloRemtingService)Activator.GetObject(
+              typeof(IHelloRemotingService.IHelloRemtingService),
+              "tcp://localhost:8080/GetMessage");
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = client.GetMessage(TextBox1.Text);
+        }
+
+    }
+}
---
+++ WCF/WebServicesDemo/HelloWebService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServicesDemo
+{
+    /// <summary>
+    /// Descrizione di riepilogo per HelloWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Per consentire la chiamata di questo servizio Web dallo script utilizzando ASP.NET AJAX, rimuovere il commento dalla riga seguente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class HelloWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string GetMessage(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
